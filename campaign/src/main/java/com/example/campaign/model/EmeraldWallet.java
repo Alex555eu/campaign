3ad03cc1ac59ca0f180cf8be8aa1a4698968219b
Alex555eu <aleksanderpasichofficial@gmail.com>
2025-03-22 21:40:11 +0100

    Added EmeraldWallet model

---
+++ campaign/src/main/java/com/example/campaign/model/EmeraldWallet.java
@@ -0,0 +1,28 @@
+package com.example.campaign.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class EmeraldWallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private BigDecimal balance;
+
+    @OneToOne
+    private User user;
+}
