cbda3e8992e29811690dd6b4d6e19bcec70c5c02
Alex555eu <aleksanderpasichofficial@gmail.com>
2025-03-23 17:19:24 +0100

    Pre-populating keywords and town data

--- campaign/src/main/java/com/example/campaign/StartupApplicationListener.java
+++ campaign/src/main/java/com/example/campaign/StartupApplicationListener.java
@@ -1,11 +1,16 @@
 package com.example.campaign;
 
 import com.example.campaign.model.EmeraldWallet;
+import com.example.campaign.model.Keyword;
+import com.example.campaign.model.Town;
 import com.example.campaign.model.User;
 import com.example.campaign.repository.EmeraldWalletRepository;
+import com.example.campaign.repository.KeywordRepository;
+import com.example.campaign.repository.TownRepository;
 import com.example.campaign.repository.UserRepository;
 import com.example.campaign.service.UserService;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.lang.NonNull;
@@ -13,6 +18,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @AllArgsConstructor
 @Component
@@ -20,8 +28,11 @@ public class StartupApplicationListener implements ApplicationListener<Applicati
 
     private final UserRepository userRepository;
     private final EmeraldWalletRepository emeraldWalletRepository;
+    private final KeywordRepository keywordRepository;
+    private final TownRepository townRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @SneakyThrows
     @Override
     public void onApplicationEvent(@NonNull ApplicationReadyEvent event) {
 
@@ -37,6 +48,36 @@ public class StartupApplicationListener implements ApplicationListener<Applicati
                 .emeraldWallet(emeraldWallet)
                 .build();
         userRepository.save(user);
+
+        try (Stream<String> lines = Files.lines(Paths.get("src/main/resources/keywords.txt"))) {
+            lines
+                    .map(String::trim)
+                    .filter(line -> !line.isEmpty())
+                    .forEach(this::loadKeywordToDatabase);
+        }
+
+        try (Stream<String> lines = Files.lines(Paths.get("src/main/resources/towns.txt"))) {
+            lines
+                    .map(String::trim)
+                    .filter(line -> !line.isEmpty())
+                    .forEach(this::loadTownToDatabase);
+        }
+
+
+    }
+
+    private void loadKeywordToDatabase(String value) {
+        Keyword keyword = Keyword.builder()
+                .keyword(value)
+                .build();
+        keywordRepository.save(keyword);
+    }
+
+    private void loadTownToDatabase(String value) {
+        Town town = Town.builder()
+                .name(value)
+                .build();
+        townRepository.save(town);
     }
 
 }
