480ed66501e4d44e653d2fd2346e0894e76a4ddc
Alex555eu <aleksanderpasichofficial@gmail.com>
2025-03-23 13:44:08 +0100

    Updated Exception handling

--- campaign/src/main/java/com/example/campaign/StartupApplicationListener.java
+++ campaign/src/main/java/com/example/campaign/StartupApplicationListener.java
@@ -1,6 +1,8 @@
 package com.example.campaign;
 
+import com.example.campaign.model.EmeraldWallet;
 import com.example.campaign.model.User;
+import com.example.campaign.repository.EmeraldWalletRepository;
 import com.example.campaign.repository.UserRepository;
 import com.example.campaign.service.UserService;
 import lombok.AllArgsConstructor;
@@ -10,21 +12,29 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 @AllArgsConstructor
 @Component
 public class StartupApplicationListener implements ApplicationListener<ApplicationReadyEvent> {
 
     private final UserRepository userRepository;
-    private final UserService userService;
+    private final EmeraldWalletRepository emeraldWalletRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Override
     public void onApplicationEvent(@NonNull ApplicationReadyEvent event) {
 
+        EmeraldWallet emeraldWallet = EmeraldWallet.builder()
+                .balance(new BigDecimal("500.0"))
+                .build();
+        emeraldWalletRepository.save(emeraldWallet);
+
         User user = User.builder()
                 .emailAddress("user@email.com")
                 .firstName("user")
                 .password(passwordEncoder.encode("user"))
+                .emeraldWallet(emeraldWallet)
                 .build();
         userRepository.save(user);
     }
--- campaign/src/main/java/com/example/campaign/controller/GlobalExceptionHandler.java
+++ campaign/src/main/java/com/example/campaign/controller/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.campaign.controller;
 
 import com.example.campaign.exception.AuthenticationException;
+import com.example.campaign.exception.EntityNotFoundException;
+import com.example.campaign.exception.NegativeBalanceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,6 +15,27 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleAuthentication(AuthenticationException ex) {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NegativeBalanceException.class)
+    public ResponseEntity<String> handleNegativeBalance(NegativeBalanceException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFound(EntityNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ex.getMessage());
     }
 
