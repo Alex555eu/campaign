c2b2f01422a1f8898a1766b3b7dc5f536c92253c
Alex555eu <aleksanderpasichofficial@gmail.com>
2025-03-24 16:07:07 +0100

    Modified routing

--- angular-campaign/src/app/app.routes.ts
+++ angular-campaign/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { MainBoardComponent } from './components/main-board/main-board.component';
+import { RedirectComponent } from './components/redirect/redirect.component';
 
 export const routes: Routes = [
     { path: '', component: MainBoardComponent},
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
+    { path: 'redirect', component: RedirectComponent},
     { path: '**', redirectTo: 'home'}
 
 ];
